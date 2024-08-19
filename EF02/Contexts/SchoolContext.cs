0804c9b3b5b20fcd262bc49e3b636f59caffddd4
haboa <haboa@ASUSTUF>
2024-08-19 23:14:12 +0300

    Done

--- EF02/Contexts/SchoolContext.cs
+++ EF02/Contexts/SchoolContext.cs
@@ -18,28 +18,116 @@ namespace EF02.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Student → Department (Many-to-One)
+            // Configure Student entity
+            modelBuilder.Entity<Student>()
+                .HasKey(s => s.ID);
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.FName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.LName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.Address)
+                .IsRequired();
+
+            modelBuilder.Entity<Student>()
+                .Property(s => s.Age)
+                .IsRequired();
+
             modelBuilder.Entity<Student>()
                 .HasOne(s => s.Department)
-                .WithMany(d => d.Students)
+                .WithMany(d => d.Students)  // Department has many Students
                 .HasForeignKey(s => s.Dep_Id);
 
-            // Department → Instructor (One-to-Many)
+            // Configure Department entity
+            modelBuilder.Entity<Depratment>()
+                .HasKey(d => d.ID);
+
+            modelBuilder.Entity<Depratment>()
+                .Property(d => d.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Depratment>()
+                .Property(d => d.HiringDate)
+                .IsRequired();
+
             modelBuilder.Entity<Depratment>()
                 .HasMany(d => d.Instructors)
                 .WithOne(i => i.Department)
                 .HasForeignKey(i => i.Dept_ID);
 
-            // Instructor → Department (Many-to-One)
+            modelBuilder.Entity<Depratment>()
+                .HasMany(d => d.Students)
+                .WithOne(s => s.Department)
+                .HasForeignKey(s => s.Dep_Id);
+
+            // Configure Instructor entity
+            modelBuilder.Entity<Instructor>()
+                .HasKey(i => i.ID);
+
+            modelBuilder.Entity<Instructor>()
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Instructor>()
+                .Property(i => i.Salary)
+                .HasColumnType("double");
+
+            modelBuilder.Entity<Instructor>()
+                .Property(i => i.Bonus)
+                .HasColumnType("double");
+
+            modelBuilder.Entity<Instructor>()
+                .Property(i => i.HourRate)
+                .HasColumnType("double");
+
             modelBuilder.Entity<Instructor>()
                 .HasOne(i => i.Department)
                 .WithMany(d => d.Instructors)
                 .HasForeignKey(i => i.Dept_ID);
 
-            // Course → Topic (Many-to-One)
+            // Configure Course entity
+            modelBuilder.Entity<Course>()
+                .HasKey(c => c.ID);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Duration)
+                .IsRequired();
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsRequired();
+
             modelBuilder.Entity<Course>()
                 .HasOne(c => c.Topic)
-                .WithMany(t => t.Courses)
+                .WithMany(t => t.Courses)  // Topic has many Courses
+                .HasForeignKey(c => c.Top_ID);
+
+            // Configure Topic entity
+            modelBuilder.Entity<Topic>()
+                .HasKey(t => t.ID);
+
+            modelBuilder.Entity<Topic>()
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Topic>()
+                .HasMany(t => t.Courses)
+                .WithOne(c => c.Topic)
                 .HasForeignKey(c => c.Top_ID);
         }
     }
--- EF02/Program.cs
+++ EF02/Program.cs
@@ -1,5 +1,5 @@
 ﻿using EF02.Contexts;
-
+using EF02.Entities;
 namespace EF02
 {
     internal class Program
@@ -7,7 +7,29 @@ namespace EF02
         static void Main(string[] args)
         {
             //By convention
-            ITIDbContext context = new ITIDbContext();
+            //ITIDbContext context = new ITIDbContext();
+
+            //CRUD
+            using ( var context = new SchoolContext())
+            {
+                // Create
+                var department = new Depratment { Name = "Computer Science", HiringDate = "2024-08-19" };
+                context.Departments.Add(department);
+                context.SaveChanges();
+
+                // Read
+                var students = context.Students.ToList();
+
+                // Update
+                var student = context.Students.First();
+                student.FName = "UpdatedName";
+                context.SaveChanges();
+
+                // Delete
+                var instructor = context.Instructors.First();
+                context.Instructors.Remove(instructor);
+                context.SaveChanges();
+            }
         }
     }
 }
